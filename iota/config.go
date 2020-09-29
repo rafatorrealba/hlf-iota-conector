5d955035ad8aed6390adcd748bf1e2cfd8f5d896
ubiot-alejandro <ubiot.alejandro@gmail.com>
2020-09-29 09:09:18 -0400

    HLF and IOTA connector

---
+++ iota/config.go
@@ -0,0 +1,20 @@
+package iota
+
+const endpoint = "https://nodes.comnet.thetangle.org:443"
+
+// difficulty of the proof of work required to attach a transaction on the tangle
+const mwm = 10
+
+// how many milestones back to start the random walk from
+const depth = 3
+
+const transactionTag = "UBIOT"
+
+const MamMode = "public" // "restricted" "private"
+
+const MamSideKey = ""
+
+// IOTA Wallet
+const DefaultWalletSeed = "RTZKOKTX9WMASJMXG9SGSWNGSAE9TWHACCTQNVLVR9XSDPBMZGVODEUZU9USLLKZAIOZGLSA9UBOTG9LQ"
+const KeyIndex = 0
+const DefaultAmount = 100
